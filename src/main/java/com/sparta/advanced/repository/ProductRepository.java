bf8fd29abd219d53211cb75016f6c5c946e0a53f
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-05 02:17:44 +0900

    10_3 상품조회(관리자용)

--- src/main/java/com/sparta/advanced/repository/ProductRepository.java
+++ src/main/java/com/sparta/advanced/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.sparta.advanced.repository;
 import com.sparta.advanced.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByUserId(Long userId);
 }
