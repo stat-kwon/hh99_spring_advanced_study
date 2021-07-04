18d320f261c466cc2789e4a41e436a8498826424
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-05 02:10:24 +0900

    상품 등록 (회원별) findAllByUserID를 추가해 줌으로써 ID로 회원 정보를 찾는 것을 설정할 수 있음

--- src/main/java/com/sparta/advanced/repository/ProductRepository.java
+++ src/main/java/com/sparta/advanced/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.sparta.advanced.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUserId(Long userId);
 }
