d6aa5e2aaf773abdc07609fb3d85a7e6c3adb544
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-05 17:28:30 +0900

    (3주차) Test
    Test입니다.

--- src/test/java/com/sparta/advanced/service/ProductServiceTest.java
+++ src/test/java/com/sparta/advanced/service/ProductServiceTest.java
@@ -7,10 +7,17 @@ import com.sparta.advanced.model.Product;
 import com.sparta.advanced.repository.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
     @Mock
     ProductRepository productRepository;
@@ -37,6 +44,8 @@ class ProductServiceTest {
         Product product = new Product(requestProductDto, userId);
 
         ProductService productService = new ProductService(productRepository);
+        when(productRepository.findById(productId))
+                .thenReturn(Optional.of(product));
 
         // when
         Product result = productService.updateProduct(productId, requestMyPriceDto);
@@ -45,4 +54,35 @@ class ProductServiceTest {
         assertEquals(myprice, result.getMyprice());
     }
 
+    @Test
+    @DisplayName("updateProduct() 에 의해 관심 가격이 100원 이하인 경우 에러 발생")
+    void updateProduct_abnormal() {
+        // given
+        Long productId = 100L;
+        int myprice = 50;
+
+        ProductMypriceRequestDto requestMyPriceDto = new ProductMypriceRequestDto(
+                myprice
+        );
+
+        Long userId = 12345L;
+        ProductRequestDto  requestProductDto = new ProductRequestDto(
+                "오리온 꼬북칩 초코츄러스맛 160g",
+                "https://shopping-phinf.pstatic.net/main_2416122/24161228524.20200915151118.jpg",
+                "https://search.shopping.naver.com/gate.nhn?id=24161228524",
+                2350
+        );
+
+        Product product = new Product(requestProductDto, userId);
+
+        ProductService productService = new ProductService(productRepository);
+        when(productRepository.findById(productId))
+                .thenReturn(Optional.of(product));
+
+        // when
+        // then
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            productService.updateProduct(productId, requestMyPriceDto);
+        });
+    }
 }
