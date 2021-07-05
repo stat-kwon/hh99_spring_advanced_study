eef2faad21a7f3cd2230f64643fb67c59ec94940
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-05 11:06:19 +0900

    접근 불가 페이지 만들기 @EnableGlobalMethodSecurity 추후 할 것을 위해 넣어줘야함. 맨 마지막 줄에 인가되지 않는다면 forbidden.html로 가게되는 것을 넣어줌

--- src/main/java/com/sparta/advanced/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/advanced/security/WebSecurityConfig.java
@@ -1,7 +1,8 @@
-package com.sparta.advanced.security;
+package com.sparta.springcore.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,8 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
@@ -18,29 +19,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();                     // 해커로 부터 방어
-        http.headers().frameOptions().disable();   //
-
-        // 로그인 기능 상세 설정을 해주는 것것 ( 접근 권한을 설정해 주는 것 )
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
         http.authorizeRequests()
                 // image 폴더를 login 없이 허용
                 .antMatchers("/images/**").permitAll()
                 // css 폴더를 login 없이 허용
                 .antMatchers("/css/**").permitAll()
-                .antMatchers("/user/**").permitAll()  // user로 시작되는 부분은 인증과정 없이 처리가 되도록 하는 것
-                .antMatchers("/h2-console/**").permitAll()  // user로 시작되는 부분은 인증과정 없이 처리가 되도록 하는 것
+                .antMatchers("/user/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 // 그 외 모든 요청은 인증과정 필요
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/user/login")                // 로그인 페이지 호출을 설정
-                .loginProcessingUrl("/user/login")       // 실제 로그인을 진행
-                .defaultSuccessUrl("/")                  // 로그인 생성시 보여줄 페이지
+                .loginPage("/user/login")
+                .loginProcessingUrl("/user/login")
+                .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
                 .logout()
                 .logoutUrl("/user/logout")
-                .permitAll();
+                .permitAll()
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/user/forbidden");
     }
 }
-
